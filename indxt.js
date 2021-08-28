4c1797c512c8c17100cb2fafc736a62b513d90d0
Emon191217 <smashrafeahmed@gmail.com>
2021-08-28 21:22:45 +0600

    first commit

---
+++ indxt.js
@@ -0,0 +1,13 @@
+// ;;;;;;;;;;;;;;;;;;;add;;;;;;;;;;;;;;;;;;;;;;
+var nambar = 2;
+while (nambar <= 20) {
+    console.log(nambar);
+    nambar = nambar + 2;
+}
+
+// ;;;;;;;;;;;;;;;;bijor nambar;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+var nambar = 1;
+while (nambar <= 15) {
+    console.log(nambar);
+    nambar = nambar + 2;
+}
